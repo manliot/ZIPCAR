3ccba58420cf965c662de5641d4be5d6cf213808
manlio <manliot@uninorte.edu.co>
2020-05-21 13:33:02 -0500

    ajustado efecto del navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -18,18 +18,18 @@ class NavigatorBar extends Component {
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
-                                <Link className="Nav-link" to="/QueesZipcar">¿Que es ZIPCAR? <span className="sr-only">(current)</span></Link>
+                    <ul className="navbar-nav mr-auto">
+                            <li className="nav-item dropdown">
+                                <NavDropdown title="Informacion" id="navbarDropdown">
+                                    <NavDropdown.Item href="/QueesZipcar">Que es Cioscar</NavDropdown.Item>
+                                    <NavDropdown.Item href="#comotrabajamos">¿Como Trabajamos?</NavDropdown.Item>
+                                </NavDropdown>
                             </li>
                             <li className="nav-item">
                                 <Link className="Nav-link" to="#precios">Precios</Link>
                             </li>
                             <li className="nav-item">
                                 <Link className="Nav-link" to="/Disponibles">Disponibles</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link className="Nav-link" to="#comotrabajamos">¿Como trabajamos?</Link>
                             </li>
                             <li className="nav-item dropdown">
                                 <NavDropdown title="Servicios" id="navbarDropdown">
@@ -39,10 +39,6 @@ class NavigatorBar extends Component {
                                 </NavDropdown>
                             </li>
                         </ul>
-                        <form className="form-inline my-2 my-lg-0">
-                            <input className="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search" />
-                            <button className="btn btn-outline-success my-2 my-sm-0" id='boton-buscar' type="submit">Buscar</button>
-                        </form>
                         {this.props.usuarioLog === '' && (
                             <ul className="navbar-nav ">
                                 <li className="nav-item">
@@ -60,7 +56,7 @@ class NavigatorBar extends Component {
 
                     </div>
                 </nav>
-                <img src={require("../Images/efecto.png")} height={170} id='imagenefecto' alt="efecto" />
+                <img src={require("../Images/efecto.png")} height={600} id='imagenefecto' alt="efecto" />
             </div>
         )
     }
