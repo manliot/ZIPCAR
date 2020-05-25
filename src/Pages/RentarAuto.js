5b733bbae43d57504ceabee3a24566dd7f328562
manlio <manliot@uninorte.edu.co>
2020-05-25 03:42:55 -0500

    acomodando elementos de rentar auto

--- src/Pages/RentarAuto.js
+++ src/Pages/RentarAuto.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 //import '..Styles/RentarAuto.css'
 import Dispo from '../Components/DIsponiblescomponent'
 import Navjs from '../Components/Navbar'
+import Footer from '../Components/Footer'
 import '../Styles/Home.css'
 export default class RentarAuto extends Component {
     render() {
         return (
-            <div>
-                <Navjs id='navibari' ></Navjs>
-                < Dispo></Dispo>
-                
+            <div className='contenedor_total'>
+                <Navjs id='navibari'></Navjs>
+                <div className='subBody'>
+                    <div className='subsubbody2' >
+                        < Dispo></Dispo>
+
+                    </div>
+                    <div className='keren'>
+                        <Footer />
+                    </div>
+                </div>
             </div>
+
+
+
         )
     }
 }
